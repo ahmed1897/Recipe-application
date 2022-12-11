2670f9a3f07af8fa19cba309100540b5bffe323c
ahmed1897 <ahmed1897@gmail.com>
2022-12-11 11:57:29 +0200

    Last touches

--- src/Components/Veggie.jsx
+++ src/Components/Veggie.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Link } from "react-router-dom";
 import "@splidejs/splide/dist/css/splide.min.css";
 function Veggie() {
   const [veggie, setVeggie] = useState([]);
@@ -40,9 +41,11 @@ function Veggie() {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
                 </Card>
               </SplideSlide>
             );
--- src/Pages/Cuisine.jsx
+++ src/Pages/Cuisine.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 //import {motion} from 'framer-motion';
 import { Link, useParams } from "react-router-dom";
-
+import { motion } from "framer-motion";
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
   let params = useParams();
@@ -21,19 +21,26 @@ function Cuisine() {
   console.log(cuisine);
 
   return (
-    <Grid>
+    <Grid 
+    animate={{ opacity: 1 }}
+    initial={{ opacity: 0 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
-            <img src={item.image} alt="" />
-            <h4>{item.title}</h4>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Link>
           </Card>
         );
       })}
     </Grid>
   );
 }
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
